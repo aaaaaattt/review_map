f4cd43d0cc12d10a2d9c9b2c00207bfcc396f746
ch <chlgur13@kakao.com>
2024-11-28 20:24:34 +0900

    fix3

--- place_recommendation_app_v2.py
+++ place_recommendation_app_v2.py
@@ -11,11 +11,17 @@ from streamlit_folium import st_folium
 from dotenv import load_dotenv
 import os
 import time
+import streamlit as st
 
 # .env 파일 로드
 load_dotenv()
-openai.api_key = os.getenv("OPENAI_API_KEY")
-google_maps_api_key = os.getenv("GOOGLE_MAPS_API_KEY")
+# Secrets에서 API 키 가져오기
+openai_api_key = st.secrets["OPENAI_API_KEY"]
+google_maps_api_key = st.secrets["GOOGLE_MAPS_API_KEY"]
+
+# 확인용 출력 (배포된 앱에서는 민감한 정보 노출 금지)
+st.write(f"OpenAI API Key: {openai_api_key[:5]}******")
+st.write(f"Google Maps API Key: {google_maps_api_key[:5]}******")
 
 # FAISS 및 데이터 로드
 faiss_index_path = "./faiss_index.bin"  # 저장된 FAISS 파일 경로
